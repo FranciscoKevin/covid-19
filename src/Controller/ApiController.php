63c3b2e0da72722704d5f7ffe60a91ebe373e622
FrKev1 <francisco.kevin37@gmail.com>
2022-02-20 21:07:06 +0100

    Endpoint with departments and create view

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,11 +10,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiController extends AbstractController
 {
     #[Route('/', name: 'api')]
-    public function index(CallApiService $callApiService): Response
+    public function getAllFrance(CallApiService $callApiService): Response
     {
-        //dd($callApiService->getFranceData());
-        return $this->render('api/index.html.twig', [
+        //dd($callApiService->getAllDepartments());
+        return $this->render('api/france.html.twig', [
             'data' => $callApiService->getFranceData(),
+        ]);
+    }
+
+    #[Route('/tous-les-departements', name: 'departments')]
+    public function getAllDepartments(CallApiService $callApiService): Response
+    {
+        //dd($callApiService->getAllDepartments());
+        return $this->render('api/all_departments.html.twig', [
+            'departments' => $callApiService->getAllDepartments(),
+        ]);
+    }
+
+    #[Route('/departement/{department}', name: 'department')]
+    public function getByDepartment(string $department, CallApiService $callApiService): Response
+    {
+        //dd($callApiService->getDepartment($department));
+        return $this->render('api/by_department.html.twig', [
+            'department' => $callApiService->getDepartment($department),
         ]);
     }
 }
